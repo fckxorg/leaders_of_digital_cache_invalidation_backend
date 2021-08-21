531401c210f1abb4eeca7a36587455c9371c61f2
BorisTab <boristab23@gmail.com>
2021-08-21 04:08:23 +0300

    frontend: add bloger's cards
    This commit adds filter for bloger's cards and bloger's cards. Request to the server is not ready yet.

--- searcher/api/static/js/main.js
+++ searcher/api/static/js/main.js
@@ -1,1 +1,31 @@
-alert("Hello, world!")
+document.addEventListener('DOMContentLoaded', function () {
+    $('.filter-button').on('click', () => {
+        let filter_form = {
+          query: $('.query-input').val(),
+          subs_lo: $('.min-sub-input').val(),
+          subs_hi: $('.max-sub-input').val(),
+          avg_likes: $('.avg-likes-input').val(),
+          avg_views: $('.avg-views-input').val(),
+        };
+    
+        console.log(filter_form);
+
+        fetch('/bloger/search', {
+            method: 'post',
+            body: JSON.stringify(filter_form)
+        })
+        .then(response => {
+            if (response.status !== 200) {
+                console.log("Nothing. Status: " + response.status);
+                return;
+            }
+    
+            response.json().then(data => {
+                console.log(data);
+            });
+        })
+        .catch(error => {
+            console.log("Fetch error: -S", error);
+        });
+    });
+});
