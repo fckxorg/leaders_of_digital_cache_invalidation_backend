041170c098860238e3cdaeb36ef533f93f306f56
BorisTab <boristab23@gmail.com>
2021-08-21 15:43:44 +0300

    frontend: add email form
    This commit add preporation of the form for email sending.
    Also add checbox for bloger's cards

--- searcher/api/static/js/main.js
+++ searcher/api/static/js/main.js
@@ -30,34 +30,39 @@ document.addEventListener('DOMContentLoaded', function () {
 
                     bloger_cards_html += `
                     <div class="bloger-card bg-white p-3 w-full flex flex-col rounded-md dark:bg-gray-800 shadow">
-                    <div class="flex xl:flex-row flex-col items-center font-medium text-gray-900 dark:text-white pb-2 mb-2 xl:border-b border-gray-200 border-opacity-75 dark:border-gray-700 w-full">
-                        <img src="${bloger.photo}" class="w-7 h-7 mr-2 rounded-full" alt="profile" />
-                        <a href="${bloger.link}" target="_blank">${bloger.name}</a>
-                    <div class="network">
-                    <div class="text-gray-400">${bloger.welness}</div>
-                        ${network_icon(bloger.network)}
+                        <div class="flex xl:flex-row flex-col items-center font-medium text-gray-900 dark:text-white pb-2 mb-2 xl:border-b border-gray-200 border-opacity-75 dark:border-gray-700 w-full">
+                            <img src="${bloger.photo}" class="w-7 h-7 mr-2 rounded-full" alt="profile" />
+                            <a href="${bloger.link}" target="_blank">${bloger.name}</a>
+                        <div class="network">
+                        <div class="text-gray-400">${bloger.welness}</div>
+                            ${network_icon(bloger.network)}
+                            </div>
                         </div>
-                    </div>
-                    <div class="flex items-center w-full margin-down-5">
-                        <div class="text-xs py-1 px-2 leading-none dark:bg-gray-900 bg-blue-100 text-blue-500 rounded-md">Почта</div>
-                        <div class="ml-auto text-xs text-gray-500">${bloger.email}</div>
-                    </div>
-                    <div class="flex items-center w-full margin-down-5">
-                        <div class="text-xs py-1 px-2 leading-none dark:bg-gray-900 bg-green-100 text-green-600 rounded-md">Телефон</div>
-                        <div class="ml-auto text-xs text-gray-500">${bloger.phone}</div>
-                    </div>
-                    <div class="flex items-center w-full margin-down-5">
-                        <div class="text-xs py-1 px-2 leading-none dark:bg-gray-900 bg-yellow-100 text-yellow-600 rounded-md">Подписчики</div>
-                        <div class="ml-auto text-xs text-gray-400">${bloger.subs}</div>
-                    </div>
-                    <div class="flex items-center w-full margin-down-5">
-                        <div class="text-xs py-1 px-2 leading-none dark:bg-gray-900 bg-yellow-100 text-yellow-600 rounded-md">Лайки</div>
-                        <div class="ml-auto text-xs text-gray-400">${bloger.avg_likes}</div>
-                    </div>
-                    <div class="flex items-center w-full margin-down-5">
-                        <div class="text-xs py-1 px-2 leading-none dark:bg-gray-900 bg-yellow-100 text-yellow-600 rounded-md">Просмотры</div>
-                        <div class="ml-auto text-xs text-gray-400">${bloger.avg_view}</div>
-                    </div>
+                        <div class="flex items-center w-full margin-down-5">
+                            <div class="text-xs py-1 px-2 leading-none dark:bg-gray-900 bg-blue-100 text-blue-500 rounded-md">Почта</div>
+                            <div class="ml-auto text-xs text-gray-500">${bloger.email}</div>
+                        </div>
+                        <div class="flex items-center w-full margin-down-5">
+                            <div class="text-xs py-1 px-2 leading-none dark:bg-gray-900 bg-blue-100 text-blue-500 rounded-md">Телефон</div>
+                            <div class="ml-auto text-xs text-gray-500">${bloger.phone}</div>
+                        </div>
+                        <div class="flex items-center w-full margin-down-5">
+                            <div class="text-xs py-1 px-2 leading-none dark:bg-gray-900 bg-yellow-100 text-yellow-600 rounded-md">Подписчики</div>
+                            <div class="ml-auto text-xs text-gray-400">${bloger.subs}</div>
+                        </div>
+                        <div class="flex items-center w-full margin-down-5">
+                            <div class="text-xs py-1 px-2 leading-none dark:bg-gray-900 bg-yellow-100 text-yellow-600 rounded-md">Лайки</div>
+                            <div class="ml-auto text-xs text-gray-400">${bloger.avg_likes}</div>
+                        </div>
+                        <div class="flex items-center w-full margin-down-5">
+                            <div class="text-xs py-1 px-2 leading-none dark:bg-gray-900 bg-yellow-100 text-yellow-600 rounded-md">Просмотры</div>
+                            <div class="ml-auto text-xs text-gray-400">${bloger.avg_view}</div>
+                        </div>
+
+                        <div class="flex items-center w-full">
+                            <div class="text-xs py-1 px-2 leading-none dark:bg-gray-900 bg-green-100 text-green-600 rounded-md">Выбрать</div>
+                            <input type="checkbox" class="ml-auto text-xs text-gray-400"></input>
+                        </div>
                     </div>
                 `;
                 });
